f8217e68d3849d962bcd840bf750167accc135a6
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-04-13 22:26:05 +0500

    fix: type errors in fetch api

--- apps/web/src/lib/fetch.ts
+++ apps/web/src/lib/fetch.ts
@@ -1,6 +1,6 @@
-const fetchAbsolute = (fetch) => {
-  return (baseUrl) =>
-    (url, ...otherParams) =>
+const fetchAbsolute = (fetch: typeof window.fetch) => {
+  return (baseUrl: string) =>
+    (url: string, ...otherParams: RequestInit[]) =>
       url.startsWith("/")
         ? fetch(baseUrl + url, ...otherParams)
         : fetch(url, ...otherParams);
