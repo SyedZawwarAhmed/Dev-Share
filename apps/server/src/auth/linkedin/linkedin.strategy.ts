2478b4f4e56a882754625ca3d5dafab529245a80
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-05-02 02:03:22 +0500

    feat(auth): setup linkedin oauth on the server

---
+++ apps/server/src/auth/linkedin/linkedin.strategy.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-linkedin-oauth2';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LinkedinStrategy extends PassportStrategy(Strategy, 'linkedin') {
+  constructor(private configService: ConfigService) {
+    super({
+      clientID: configService.get('LINKEDIN_CLIENT_ID'),
+      clientSecret: configService.get('LINKEDIN_CLIENT_SECRET'),
+      callbackURL: `${configService.get('LINKEDIN_CALLBACK_URL')}`,
+      scope: ['w_member_social', 'openid', 'profile', 'email'],
+      state: true,
+      profileFields: [
+        'id',
+        'first-name',
+        'last-name',
+        'email-address',
+        'profile-picture',
+      ],
+      auth: {
+        auth_type: 'authenticate',
+      },
+      passReqToCallback: false,
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: Function,
+  ) {
+    const { name, email, givenName, familyName, displayName, picture } =
+      profile;
+    const user = {
+      email,
+      firstName: givenName,
+      lastName: familyName,
+      profileImage: picture,
+      accessToken, // Store this for making LinkedIn API calls
+      refreshToken,
+    };
+    done(null, user);
+  }
+}
