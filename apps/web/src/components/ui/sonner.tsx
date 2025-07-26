a5c6657caea18f19cbbd1d309a4d89db567afd6b
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-07-26 15:30:58 +0500

    fix: toast description color

--- apps/web/src/components/ui/sonner.tsx
+++ apps/web/src/components/ui/sonner.tsx
@@ -8,6 +8,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      toastOptions={{
+        classNames: {
+          description: '!text-neutral-700'
+        }
+      }}
       style={
         {
           "--normal-bg": "var(--popover)",
