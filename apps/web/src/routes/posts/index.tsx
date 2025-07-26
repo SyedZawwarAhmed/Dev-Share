5e10ac96f75873aefd8f7e03f31c396fae3a330e
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-07-27 00:38:46 +0500

    fix(web): imports in posts/index.tsx file

--- apps/web/src/routes/posts/index.tsx
+++ apps/web/src/routes/posts/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
